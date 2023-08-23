d0accba5b9f0b39084585930ed5b6a7a2b4a731f
p-camila6 <138184276+p-camila6@users.noreply.github.com>
2023-08-22 21:45:49 -0300

    Create exercicio.js
    dia 22/8

---
+++ exercicio.js
@@ -0,0 +1,41 @@
+/*
+*Camila Peres
+*
+*
+*Exercício
+
+*1 - Irá aparecer no console
+
+*2 - Irá aparecer 10 10 10 
+
+*3 - let horaTrabalho = prompt("Quantas horas de...");
+* let precoDia = prompt("Quanto voce recebe...");
+*alert(`Voce recebe ${t/p} por hora`)
+
+*1.(de novo) - a, b, c 
+*/
+
+let variavel1;
+let variavelIdade;
+
+console.log(typeof variavel1);
+console.log(typeof variavelIdade);
+
+// d) Apareceu no console "undefined", isso significa que a variavel naão possui valor
+
+//e) 
+
+variavel1 =prompt("Seu nome?");
+variavelIdade =prompt("Qual sua idade?");
+
+// f)
+
+console.log(typeof variavel1);
+console.log(typeof variavelIdade);
+
+// Notei que as duas variavéis sairam como string
+
+// g)
+
+console.log("Olá, seu nome", variavel1, "e sua idaide é", variavelIdade);
+
