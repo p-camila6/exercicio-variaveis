71f0d63998fb001efd7a3eec9da8ef126973db3a
p-camila6 <138184276+p-camila6@users.noreply.github.com>
2023-08-22 21:46:32 -0300

    Create index.js
    index

---
+++ index.js
@@ -0,0 +1,48 @@
+let variavelTexto = "texto"
+let variavelNumero = 6;
+console.log(typeof variavelTexto);
+console.log(variavelNumero);
+let variavel22 = "teste"
+console.log(typeof variavel22)
+let variavel33 = (123456)
+console.log(typeof variavel33);
+
+//variavel vazia
+
+let variavelVazia;
+console.log(typeof variavelVazia);
+
+//variavel nula
+
+let variavelnula = null;
+console.log(variavelnula);
+
+//variavel
+//let a = "b"
+//console.log(a)
+//console.log(typeof a);
+//a = null
+//console.log(type a)
+
+
+
+//exercicio 2 
+let nome; 
+let idade;
+let numero = 6;
+
+nome = prompt("Qual o seu nome?");
+idade = prompt("Qual a sua idade?");
+
+console.log(typeof nome);
+console.log(typeof idade); 
+console.log(number + 5);
+console.log(number.toSrtring() + 5);
+
+
+//conversao
+
+let n = "2";
+console.log(Number(n));
+console.log(typeof n);
+console.log(typeof Number(n));
