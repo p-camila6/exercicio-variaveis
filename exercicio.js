fe0a4b8f20b3208b5d92830847dee9638f7b4760
p-camila6 <138184276+p-camila6@users.noreply.github.com>
2023-08-24 20:10:15 -0300

    Update exercicio.js

--- exercicio.js
+++ exercicio.js
@@ -37,5 +37,47 @@ console.log(typeof variavelIdade);
 
 // g)
 
-console.log("Olá, seu nome", variavel1, "e sua idaide é", variavelIdade);
+console.log("Olá, seu nome é", variavel1, "e sua idade é", variavelIdade);
+
+//* 2-a)
+
+prompt("Você gosta de frio?")
+
+prompt("Você gosta de roxo?")
+
+prompt("Você gosta de estudar?")
+
+let ask1 = prompt("Você gosta de frio?");
+let ask2 = prompt("Você gosta de roxo?");
+let ask3 = prompt("Você gosta de estudar?");
+
+console.log("Você gosta de frio?", ask1);
+console.log("Você gosta de roxo?", ask2);
+console.log("Você gosta de estudar?", ask3);
+// 
+
+let a = 10 
+let b = 25 
+//invertendo os valores
+
+let c 
+c = a
+console.log(c);
+a = b
+b = c
+console.log(a, b);
+console.log("O valor de a é igual a:", a, "O valor de b é igual a:", b);
+
+
+// adicional
+
+prompt("digite um número de 0 a 10");
+prompt("digite um número de 20 à 30");
+
+
+let valor1 = Number(prompt("digite um número de 0 a 10"));
+let valor2 = Number(prompt("digite um número de 20 à 30"));
+
+console.log(valor1 + valor2); 
+console.log(valor1 * valor2); 
 
